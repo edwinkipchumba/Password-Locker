1aa357c26b568ac0d3123b978416890dbd6811a5
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-03 22:18:17 +0300

    adding method to check correct user credentials

--- credentials_class.py
+++ credentials_class.py
@@ -10,3 +10,16 @@ class UserTest(unittest.TestCase):
         method run before each user test
         '''
         self.new_user = User("James", "Doe")
+
+    def tearDown(self):
+        '''
+        method called after each user test
+        '''
+        User.data_user = []
+
+    def test_init(self):
+        '''
+        test method to check if user class is initialized correctly
+        '''
+        self.assertTrue(self.new_user.owner,"James")
+        self.assertTrue(self.new_user.key, "Doe")
