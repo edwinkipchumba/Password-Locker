6625e91a427f86a8119c9b7231bfeaeda1cada5a
edwinkipchumba <edwin.kolem@student.moringaschool.com>
2021-09-04 09:19:56 +0300

    adding function to save and delete account

--- test_class.py
+++ test_class.py
@@ -9,3 +9,30 @@ def create_user(lock_owner, lock_key):
         '''
         new_user = User(lock_owner, lock_key)
         return new_user
+
+def save_user(data):
+    '''
+    function to save a new user
+    '''
+    data.save_user()
+
+def create_account(account, u_name, passwd):
+    '''
+    function to create a new account
+    '''
+    new_account = Credentials(account, u_name, passwd)
+    return new_account
+
+def save_accounts(credentials):
+    '''
+    function to save the new account
+    '''
+    credentials.save_account()
+
+def delete_accounts(credentials):
+    '''
+    function to delete an account
+    '''
+    credentials.delete_account()
+
+def 
